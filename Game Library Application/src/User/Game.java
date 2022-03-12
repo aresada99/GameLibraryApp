1c6cb36f6739e62b2c936bfd3201c23abf6c024c
Ares Ada Dora <ares.ada.dora@gmail.com>
2022-03-12 19:34:53 +0300

    Game Library App

---
+++ Game Library Application/src/User/Game.java
@@ -0,0 +1,23 @@
+package User;
+
+public class Game{
+	protected int game_id;
+    protected  String name;
+    protected  String description;
+    protected  String publisher;
+    protected  float price;
+    protected  float score;
+    protected  boolean is_multi;
+
+    public Game(int game_id,String name,String description,String publisher,float price,float score,boolean is_multi){
+        this.game_id = game_id;
+        this.name=name;
+        this.description=description;
+        this.publisher=publisher;
+        this.price=price;
+        this.score=score;
+        this.is_multi=is_multi;
+        } 
+
+
+}
---
+++ Game Library Application/src/User/User.java
@@ -0,0 +1,18 @@
+package User;
+
+public abstract class User {
+	public String username;
+	public String password;
+	
+	public boolean login(String username,String password) {
+		return(this.username.equals(username) && this.password.equals(password));
+			
+	};
+	
+	public void sign_up(String newUsername,String newPassword) {
+		this.username = newUsername;
+		this.password = newPassword;
+		
+	}
+	
+}
